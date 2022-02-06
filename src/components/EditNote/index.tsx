92e074492414ae974dd499458944c5f735c877d3
gustavodaltoe <gustavodaltoe@hotmail.com>
2022-02-06 17:03:06 -0300

    feat: automatictly move cursor to the untitled title

--- src/components/EditNote/index.tsx
+++ src/components/EditNote/index.tsx
@@ -1,14 +1,24 @@
+import { useEffect, useRef } from 'react';
 import { useNotes } from '../../hooks/useNotes';
 import { CreateNote } from '../CreateNote';
 import * as S from './styles';
 
 export function EditNote() {
   const notes = useNotes();
+  const titleRef = useRef<HTMLTextAreaElement>(null);
   const { current } = notes;
+  const isNoteUntitled = current?.title === 'Untitled';
+
+  useEffect(() => {
+    if (!current) return;
+    if (isNoteUntitled) {
+      titleRef.current?.focus();
+    }
+  }, [current, isNoteUntitled]);
+
   if (!current) {
     return <CreateNote />;
   }
-  const isNoteUntitled = current.title === 'Untitled';
 
   function handleTitleChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
     e.target.value = e.target.value.replace(/\n/g, '');
@@ -27,6 +37,7 @@ export function EditNote() {
       </S.Header>
       <S.Content>
         <S.TitleInput
+          ref={titleRef}
           value={isNoteUntitled ? '' : current.title}
           onChange={handleTitleChange}
           placeholder={current.title}
