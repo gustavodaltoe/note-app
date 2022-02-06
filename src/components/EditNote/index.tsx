eb3ddbcbad9701e53f63e96f45d5e146bae9b406
gustavodaltoe <gustavodaltoe@hotmail.com>
2022-02-06 16:45:20 -0300

    refactor: separate note change events into functions

--- src/components/EditNote/index.tsx
+++ src/components/EditNote/index.tsx
@@ -10,24 +10,25 @@ export function EditNote() {
     return <CreateNote />;
   }
 
+  function handleTitleChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    e.target.value = e.target.value.replace(/\n/g, '');
+    const value = e.target.value;
+    notes.update({ ...current, title: value });
+  }
+
+  function handleContentChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    notes.update({ ...current, content: e.target.value });
+  }
+
   return (
     <S.Wrapper>
       <S.Header>
         <S.Title>{current.title}</S.Title>
       </S.Header>
       <S.Content>
-        <S.TitleInput
-          value={current.title}
-          onChange={(e) => {
-            e.target.value = e.target.value.replace(/\n/g, '');
-            const value = e.target.value;
-            notes.update({ ...current, title: value });
-          }}
-        />
+        <S.TitleInput value={current.title} onChange={handleTitleChange} />
         <S.ContentInput
-          onChange={(e) => {
-            notes.update({ ...current, content: e.target.value });
-          }}
+          onChange={handleContentChange}
           placeholder="Start typing..."
           value={current.content}
         />
